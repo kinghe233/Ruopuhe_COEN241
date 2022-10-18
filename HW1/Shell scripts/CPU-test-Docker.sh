4ff00568dcad99c6607df2349f6cfb80cd1a75ff
Ruopu He <51266570+kinghe233@users.noreply.github.com>
2022-10-18 01:09:08 -0700

    Create CPU-test-Docker.sh

---
+++ HW1/Shell scripts/CPU-test-Docker.sh
@@ -0,0 +1,4 @@
+docker run --rm -it --entrypoint /bin/sh my_sysbench
+sysbench --test=cpu --cpu-max-prime=100000 --time=30 run
+sysbench --test=cpu --cpu-max-prime=300000 --time=30 run
+sysbench --test=cpu --cpu-max-prime=500000 --time=30 run
