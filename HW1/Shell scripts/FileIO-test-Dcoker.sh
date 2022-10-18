419b2348885ab01c5594b1d37b85fc10a0d381a7
Ruopu He <51266570+kinghe233@users.noreply.github.com>
2022-10-18 01:12:59 -0700

    Create FileIO-test-Dcoker.sh

---
+++ HW1/Shell scripts/FileIO-test-Dcoker.sh
@@ -0,0 +1,15 @@
+docker run --rm -it --entrypoint /bin/sh my_sysbench
+
+sysbench --threads=8 --test=fileio --file-total-size=1GB prepare 
+sysbench --threads=8 --test=fileio --file-total-size=1GB --file-test-mode=rndrw --time=30 --max-requests=0 run 
+sysbench --threads=8 --test=fileio --file-total-size=1GB cleanup
+
+sysbench --threads=8 --test=fileio --file-total-size=1GB prepare 
+sysbench --threads=8 --test=fileio --file-total-size=1GB --file-test-mode=seqwr --max-time=30 --max-requests=0 run 
+sysbench --threads=8 --test=fileio --file-total-size=1GB cleanup
+
+sysbench --threads=8 --test=fileio --file-total-size=1GB prepare 
+sysbench --threads=8 --test=fileio --file-total-size=1GB --file-test-mode=rndwr --max-time=30 --max-requests=0 run 
+sysbench --threads=8 --test=fileio --file-total-size=1GB cleanup
+
+sync && sudo purge
